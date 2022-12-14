e421ca6a9fc89c3aa4dff1d87b30cc8b379ad3cc
Harry Kreicers <hkreicers@cochlear.com>
2022-12-14 18:30:21 +1100

    2022 Q14

---
+++ 2022/Q14/part_one.js
@@ -0,0 +1,100 @@
+const {input} = require('./input');
+
+input_length = input.length
+
+// const x_range = [462, 534]
+// const y_range = [13, 171]
+
+const x_range = [450, 540]
+const y_range = [0, 180]
+
+function draw_map(map){
+    for(let i=0; i<map.length;i++){
+        row = map[i]
+        for(let j=0; j<row.length;j++){
+            process.stdout.write(row[j]);
+        }
+        process.stdout.write("\n");
+    }
+}
+
+let map = [...Array(y_range[1])].map(e => Array(x_range[1] - x_range[0]).fill('.'));
+
+console.log("Input length = " + input_length);
+
+function map_set(coord, val){
+    map[coord[1]][coord[0]-x_range[0]] = val
+}
+
+function map_get(coord){
+    return(map[coord[1]][coord[0]-x_range[0]])
+}
+
+function draw_line(line){
+    let coords = line.split(" ->")
+    // console.log(coords)
+    let coord = []
+    let last_coord = coords[0].split(',').map(x=>+x)
+    for(let i=1; i<coords.length;i++){
+        coord = coords[i].split(',').map(x=>+x)
+
+        // x_range[0] = Math.min(x_range[0], coord[0])
+        // x_range[1] = Math.max(x_range[1], coord[0])
+        // y_range[0] = Math.min(y_range[0], coord[1])
+        // y_range[1] = Math.max(y_range[1], coord[1])
+        while((last_coord[0] != coord[0]) || (last_coord[1] != coord[1])){
+            // console.log("last coord = ", last_coord, " coord = ", coord)
+            map_set(last_coord, '#')
+            if(last_coord[0] == coord[0]){
+                last_coord[1] = last_coord[1] + (last_coord[1]<coord[1] ? 1 : -1)
+            } else {
+                last_coord[0] = last_coord[0] + (last_coord[0]<coord[0] ? 1 : -1)
+            }
+        }        
+    }
+    map_set(last_coord, '#')
+}
+
+for(let i=0; i<input_length;i++){
+    draw_line(input[i])
+}
+
+// draw_map(map)
+
+let free_fall = false
+let at_rest = false
+let sand_pos = [500,0]
+
+let sand_at_rest = 0
+
+while(!free_fall){
+    sand_pos = [500, 0]
+    at_rest = false
+
+    while(!at_rest){
+
+        if(sand_pos[1] >= y_range[1]-2){
+            at_rest = true
+            free_fall = true
+        }
+
+        if(map_get([sand_pos[0], sand_pos[1]+1]) == '.'){
+            sand_pos[1] += 1
+        } else if(map_get([sand_pos[0]-1, sand_pos[1]+1]) == '.'){
+            sand_pos[1] += 1
+            sand_pos[0] -= 1
+        } else if(map_get([sand_pos[0]+1, sand_pos[1]+1]) == '.'){
+            sand_pos[1] += 1
+            sand_pos[0] += 1
+        } else {
+            map_set(sand_pos, 'o')
+            at_rest = true
+            sand_at_rest += 1
+        }
+    }
+
+}
+
+draw_map(map)
+
+console.log(sand_at_rest)
