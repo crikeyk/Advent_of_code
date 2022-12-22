04f0e4d7941bbe301892cb0f53de8acee447c974
Harry Kreicers <hkreicers@cochlear.com>
2022-12-23 09:59:29 +1100

    2022 Q18

--- 2022/Q18/part_one.js
+++ 2022/Q18/part_one.js
@@ -2,15 +2,37 @@ const {input} = require('./input');
 
 console.log(input.length)
 
-const dim = 3
+const dim = 25
 
-image = tunnel = new Array(dim).fill(0).map(() => new Array(dim).fill(0).map(() => new Array(dim).fill("")));
+image = tunnel = new Array(dim).fill(0).map(() => new Array(dim).fill(0).map(() => new Array(dim).fill(0)));
 
+surface_area = 0
 
+min = [99, 99, 99]
+max = [0, 0, 0]
 
 for(let i=0; i<input.length;i++){
+    coord = input[i].split(',').map(x => +x)
+    for(let j=0; j<3;j++){
+        coord[j] += 1
+    }
+    surface_area += 6
+    console.log(coord)
+    image[coord[0]][coord[1]][coord[2]] += 1
+    for(let j=0; j<3;j++){
+        max[j] = Math.max(max[j], coord[j])
+        min[j] = Math.min(min[j], coord[j])
 
+        if(image[coord[0]+(j==0?1:0)][coord[1]+(j==1?1:0)][coord[2]+(j==2?1:0)] != 0){
+            // console.log("Touching ", [coord[0]+(j==0?1:0)],[coord[1]+(j==1?1:0)],[coord[2]+(j==2?1:0)])
+            surface_area -= 2
+        }
+        if(image[coord[0]-(j==0?1:0)][coord[1]-(j==1?1:0)][coord[2]-(j==2?1:0)] != 0){
+            // console.log("Touching ", [coord[0]-(j==0?1:0)],[coord[1]-(j==1?1:0)],[coord[2]-(j==2?1:0)])
+            surface_area -= 2
+        }
+    }
 }
-
-
-console.log(max_coord, min_coord)
+// console.log(image)
+console.log("extent", min, max)
+console.log(surface_area)
--- 2022/Q18/part_two.js
+++ 2022/Q18/part_two.js
@@ -1,182 +1,68 @@
 const {input} = require('./input');
 
-const input_width = input[0].length
-console.log(input_width)
+console.log(input.length)
 
+const dim = 30
 
-function draw_tunnel(tunnel){
-    for(let i=tunnel.length-1; i>=0;i--){
-        row = tunnel[i]
-        process.stdout.write("|");
-        for(let j=0; j<row.length;j++){
-            process.stdout.write(row[j]);
-        }
-        process.stdout.write("|\n");
+image = tunnel = new Array(dim).fill(0).map(() => new Array(dim).fill(0).map(() => new Array(dim).fill(0)));
+
+surface_area = 0
+
+// min = [99, 99, 99]
+// max = [0, 0, 0]
+
+for(let i=0; i<input.length;i++){
+    coord = input[i].split(',').map(x => +x)
+    for(let j=0; j<3;j++){
+        coord[j] += 2
     }
-    process.stdout.write("+");
-
-    for(let j=0; j<row.length;j++){
-        process.stdout.write('-');
-    }
-    process.stdout.write("+\n");
+    image[coord[0]][coord[1]][coord[2]] += 1
 }
 
 
-function draw_tunnel_with_rock(tunnel, rock, position){
+outer_SA = 0
 
-    for(let i=tunnel.length-1; i>=0;i--){
-        row = tunnel[i]
-        process.stdout.write("|");
-        for(let j=0; j<row.length;j++){
-            try{
-                process.stdout.write(rock[i-position[1]][j-position[0]]);
-            }
-            catch {
-                process.stdout.write(row[j]);
-            }
-            // process.stdout.write(row[j]);
-        }
-        process.stdout.write("|\n");
-    }
-    process.stdout.write("+");
+for(let iter=0; iter<dim;iter++){
+    console.log("iteration ", iter)
+    for(let i=0; i<dim;i++){
+        for(let j=0; j<dim;j++){
+            for(let k=0; k<dim;k++){
+                coord = [i, j, k]
+                if(image[coord[0]][coord[1]][coord[2]] == 0){
 
-    for(let j=0; j<row.length;j++){
-        process.stdout.write('-');
-    }
-    process.stdout.write("+\n");
-}
+                    reaches_border = false
+                    num_edge_touches = 0
 
-rocks = [
-    [['#','#','#', '#']],
-    [['.','#','.'],
-     ['#','#','#'],
-     ['.','#','.']],
-    [['#','#','#'],
-     ['.','.','#'],
-     ['.','.','#']],
-    [['#'],
-     ['#'],
-     ['#'],
-     ['#']],
-    [['#', '#'],
-     ['#', '#']]
-]
+                    for(let j=0; j<3;j++){
+                        if(coord[j] == dim-1 || coord[j] == 0){
+                            reaches_border = true
+                            break
+                        }
+                        // console.log(coord, j)
+                        next_piece_up = image[coord[0]+(j==0?1:0)][coord[1]+(j==1?1:0)][coord[2]+(j==2?1:0)]
+                        next_piece_down = image[coord[0]-(j==0?1:0)][coord[1]-(j==1?1:0)][coord[2]-(j==2?1:0)]
+                        if(next_piece_down == -1 || next_piece_up == -1){
+                            reaches_border = true
+                        }
 
-function collides(tunnel, rock, position){
+                        if(next_piece_up > 0){
+                            num_edge_touches += 1
+                        }
+                        if(next_piece_down > 0){
+                            num_edge_touches += 1
+                        }  
+                    }
 
-    if(position[0] < 0 || position[1] < 0 || position[0] + rock[0].length > tunnel_width){
-        return true
-    }
-
-    for(let i=0; i<rock.length;i++){
-        for(let j=0; j<rock[i].length;j++){
-            // console.log(position[1] + i, position[0] + j, tunnel.length)
-            if(rock[i][j] == '#' && (position[1] + i < tunnel.length && tunnel[position[1] + i][position[0] + j] != '.')){
-                return true
-            }
-        }
-    }
-    return false
-}
-
-function add_rock(tunnel, rock, position, char){
-    for(let i=0; i<rock.length;i++){
-        for(let j=0; j<rock[i].length;j++){
-            if(rock[i][j] == '#'){
-                tunnel[position[1] + i][position[0] + j] = char            
+                    if(reaches_border){
+                        // console.log("reaches_border")
+                        image[coord[0]][coord[1]][coord[2]] = -1
+                        outer_SA += num_edge_touches
+                    }
+                }
             }
         }
     }
 }
-const tunnel_width = 7
-const num_rocks = 1000000000
-let highest_rock = -1
-offset = 0
-input_num = 0
-shift_val = input_width*rocks.length
-last_highest = 0
-
-repeats = []
-repeat_num = 1
-
-console.log("shift value = ", shift_val)
-
-tunnel = new Array(3).fill(0).map(() => new Array(tunnel_width).fill("."));
 
 
-for(let rock_number=0; rock_number<num_rocks;rock_number++){
-
-    if(rock_number % shift_val == 0){
-        added_height = highest_rock-last_highest
-        last_highest = highest_rock
-        // console.log(added_height)
-        repeats.push(added_height)
-    }
-
-    if(rock_number % 10000000 == 0){
-        console.log(repeats)
-        // for(let i=1; i<1000;i++){
-        //     pattern = true
-        //         for(let j=1; j<50;j++){
-        //             if(repeats[repeats.length-i] != repeats[repeats.length-(j*i)]){
-        //                 pattern = false
-        //                 break
-        //             }
-        //         }
-        //         if(pattern){
-        //             console.log("Pattern found of length ", i)
-        //             break
-        //         }
-        // }
-    }
-
-    settled = false
-    rock_position = [2, highest_rock+4]
-    rock = rocks[rock_number%rocks.length]
-    while(rock_position[1] + rock.length-1 >= tunnel.length){
-        tunnel.push(new Array(tunnel_width).fill("."))
-    }
-
-    while(!settled){
-        // draw_tunnel_with_rock(tunnel, rock, rock_position)
-
-        gas_move = input[0][input_num%input_width] == '>' ? 1:-1
-        input_num++
-
-        next_position = [rock_position[0] + gas_move, rock_position[1]]
-        if(!collides(tunnel, rock, next_position)){
-            rock_position[0] = next_position[0]
-            // console.log("slide,", gas_move)
-        } else {
-            // console.log("no slide")
-        }
-        // draw_tunnel_with_rock(tunnel, rock, rock_position)
-
-
-        next_position = [rock_position[0], rock_position[1]-1]
-        if(!collides(tunnel, rock, next_position)){
-            rock_position[1] = next_position[1]
-            // console.log("fall")
-        } else {
-            add_rock(tunnel, rock, rock_position, '@')
-            settled = true
-            highest_rock = Math.max(highest_rock, rock_position[1]+rock.length-1)
-            // console.log(highest_rock)
-            // console.log("no fall")
-            break
-        }
-
-    }
-    // draw_tunnel(tunnel) 
-
-    // if(tunnel.length >= 2*shift_val){
-    //     tunnel = tunnel.slice(shift_val, -1)
-    //     offset += shift_val
-    //     highest_rock -= shift_val
-    // }
-}
-
-console.log(offset + highest_rock+1)
-
-// draw_tunnel(tunnel)
-
+console.log("outer surface area = ", outer_SA)
